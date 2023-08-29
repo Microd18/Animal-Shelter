1e25988a2ec43dc03cbfe2021736bdef65a45827
Elena Koroleva <koroliana@yandex.ru>
2023-08-30 00:10:33 +0700

    Подготовила описание для кошачьего приюта

---
+++ src/main/java/pro/sky/AnimalShelter/model/AnimalShelter.java
@@ -0,0 +1,5 @@
+package pro.sky.AnimalShelter.model;
+
+abstract public class AnimalShelter {
+    public abstract String getDescription();
+}
---
+++ src/main/java/pro/sky/AnimalShelter/model/DogShelter.java
@@ -0,0 +1,11 @@
+package pro.sky.AnimalShelter.model;
+
+public class DogShelter extends AnimalShelter{
+
+    private final String DOG_SHELTER_DESCRIPTION = "";
+
+    @Override
+    public String getDescription(){
+        return DOG_SHELTER_DESCRIPTION;
+    }
+}
