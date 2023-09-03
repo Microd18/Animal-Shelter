42431b6471a606e91618288531f321a6f30c89cc
DmitriiVolkovIzh <booyakaa@mail.ru>
2023-09-03 13:23:11 +0400

    #3 Реализовано расписание работы приюта кошек/собак
    #6 Реализовано выдача рекомендаций техники безопасности на территории приюта
    
    #8 Реализовано обращение к волонтеру, если бот не может ответить на вопросы клиента

---
+++ src/main/java/pro/sky/AnimalShelter/handlers/HelpCommandHandler.java
@@ -0,0 +1,45 @@
+package pro.sky.AnimalShelter.handlers;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pro.sky.AnimalShelter.enums.BotCommand;
+import pro.sky.AnimalShelter.state.ChatStateHolder;
+
+import static pro.sky.AnimalShelter.enums.BotCommand.*;
+@Service
+@RequiredArgsConstructor
+public class HelpCommandHandler implements CommandHandler {
+    private final ChatStateHolder chatStateHolder;
+    private final TelegramBot telegramBot;
+
+    @Override
+    public void handle(Update update) {
+        Long chatId = update.message().chat().id();
+        BotCommand currentState = chatStateHolder.getCurrentStateById(chatId);
+        if (currentState == DOG || currentState == CAT) {
+            String shelterType = currentState == DOG ? "приюте для собак" : "приюте для кошек";
+            String responseText = "Для связи с волонтером пройдите по ссылке: \n" +
+                    "\n"+
+                    "По четным дням месяца Вам поможет Дмитрий, ссылка на Телеграмм - https://t.me/DmitriyVolkov \n" +
+                    "\n"+
+                    "По нечетным дням месяца Вам поможет Елена, ссылка на Телеграмм - https://t.me/koroliana \n" +
+                    "Возврат в предыдущее меню (/back)\n" +
+                    "Выключить бота (/stop)";
+            SendMessage message = new SendMessage(chatId.toString(), responseText);
+            telegramBot.execute(message);
+            chatStateHolder.addState(chatId, BotCommand.SHELTER_INFO);
+        } else if (currentState == STOP) {
+            String responseText = "Для использования бота введите команду /start";
+            SendMessage message = new SendMessage(chatId.toString(), responseText);
+            telegramBot.execute(message);
+        }
+    }
+
+    @Override
+    public BotCommand getCommand() {
+        return HELP;
+    }
+}
