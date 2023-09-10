6a9e44908e2cae6fb6aaadb0dd699e43cad6293a
Elena Koroleva <koroliana@yandex.ru>
2023-09-11 02:02:30 +0700

    Подключение БД

--- src/main/java/pro/sky/AnimalShelter/handlers/generalHandlers/HelpCommandHandler.java
+++ src/main/java/pro/sky/AnimalShelter/handlers/generalHandlers/HelpCommandHandler.java
@@ -4,8 +4,11 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import pro.sky.AnimalShelter.enums.BotCommand;
+import pro.sky.AnimalShelter.exception.ChatStateNotFoundException;
 import pro.sky.AnimalShelter.service.ChatStateService;
 import pro.sky.AnimalShelter.state.ChatStateHolder;
 
@@ -29,6 +32,8 @@ public class HelpCommandHandler implements CommandHandler {
      */
     private final TelegramBot telegramBot;
 
+    Logger logger = LoggerFactory.getLogger(HelpCommandHandler.class);
+
     /**
      * Обрабатывает команду "/help" в зависимости от текущего состояния чата.
      *
@@ -38,25 +43,30 @@ public class HelpCommandHandler implements CommandHandler {
     public void handle(Update update) {
         Long chatId = update.message().chat().id();
         //    BotCommand currentState = chatStateHolder.getCurrentStateById(chatId);
-        BotCommand currentState = chatStateService.getCurrentStateByChatId(chatId);
-        if (currentState == DOG || currentState == CAT || currentState == SHELTER_INFO) {
-            String shelterType = currentState == DOG ? "приюте для собак" : "приюте для кошек";
-            String responseText = "Для связи с волонтером пройдите по ссылке: \n" +
-                    "\n" +
-                    "По четным дням месяца Вам поможет Дмитрий, ссылка на Телеграмм - https://t.me/DmitriyVolkov \n" +
-                    "\n" +
-                    "По нечетным дням месяца Вам поможет Елена, ссылка на Телеграмм - https://t.me/koroliana \n" +
-                    "Возврат в предыдущее меню (/back)\n" +
-                    "Выключить бота (/stop)";
-            SendMessage message = new SendMessage(chatId.toString(), responseText);
-            telegramBot.execute(message);
-            //    chatStateHolder.addState(chatId, SHELTER_INFO);
-            chatStateService.updateChatState(chatId, SHELTER_INFO);
-        } else if (currentState == STOP) {
-            String responseText = "Для использования бота введите команду /start";
-            SendMessage message = new SendMessage(chatId.toString(), responseText);
-            telegramBot.execute(message);
+        try {
+            BotCommand currentState = chatStateService.getCurrentStateByChatId(chatId);
+            if (currentState == DOG || currentState == CAT || currentState == SHELTER_INFO) {
+                String shelterType = currentState == DOG ? "приюте для собак" : "приюте для кошек";
+                String responseText = "Для связи с волонтером пройдите по ссылке: \n" +
+                        "\n" +
+                        "По четным дням месяца Вам поможет Дмитрий, ссылка на Телеграмм - https://t.me/DmitriyVolkov \n" +
+                        "\n" +
+                        "По нечетным дням месяца Вам поможет Елена, ссылка на Телеграмм - https://t.me/koroliana \n" +
+                        "Возврат в предыдущее меню (/back)\n" +
+                        "Выключить бота (/stop)";
+                SendMessage message = new SendMessage(chatId.toString(), responseText);
+                telegramBot.execute(message);
+                //    chatStateHolder.addState(chatId, SHELTER_INFO);
+                chatStateService.updateChatState(chatId, SHELTER_INFO);
+            } else if (currentState == STOP) {
+                String responseText = "Для использования бота введите команду /start";
+                SendMessage message = new SendMessage(chatId.toString(), responseText);
+                telegramBot.execute(message);
+            }
+        } catch (ChatStateNotFoundException e) {
+            logger.warn("Caught exception in HelpCommandHandler" + e.getMessage());
         }
+
     }
 
     /**
--- src/main/java/pro/sky/AnimalShelter/handlers/shelterInfoMenuHandlers/SafetyCommandHandler.java
+++ src/main/java/pro/sky/AnimalShelter/handlers/shelterInfoMenuHandlers/SafetyCommandHandler.java
@@ -4,8 +4,11 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import pro.sky.AnimalShelter.enums.BotCommand;
+import pro.sky.AnimalShelter.exception.ChatStateNotFoundException;
 import pro.sky.AnimalShelter.service.ChatStateService;
 import pro.sky.AnimalShelter.state.ChatStateHolder;
 
@@ -30,6 +33,7 @@ public class SafetyCommandHandler implements CommandHandler {
      * Экземпляр Telegram-бота для отправки сообщений.
      */
     private final TelegramBot telegramBot;
+    Logger logger = LoggerFactory.getLogger(SafetyCommandHandler.class);
 
     /**
      * Обрабатывает команду "/safety" в зависимости от текущего состояния чата.
@@ -40,38 +44,42 @@ public class SafetyCommandHandler implements CommandHandler {
     public void handle(Update update) {
         Long chatId = update.message().chat().id();
         //    BotCommand currentState = chatStateHolder.getCurrentStateById(chatId);
-        BotCommand currentState = chatStateService.getCurrentStateByChatId(chatId);
+        try {
+            BotCommand currentState = chatStateService.getCurrentStateByChatId(chatId);
+            if (currentState == SHELTER_INFO) {
+                String responseText = "Находясь на территории приюта, пожалуйста, соблюдайте наши правила и технику безопасности!\n" +
+                        "\n" +
+                        "                ЗАПРЕЩАЕТСЯ:\n" +
+                        "\n" +
+                        "- Самостоятельно открывать выгулы и вольеры без разрешения работника приюта.\n" +
+                        "\n" +
+                        "- Кормить животных. Этим Вы можете спровоцировать драку. \n" +
+                        "\n" +
+                        "- Оставлять после себя мусор на территории приюта и прилегающей территории.\n" +
+                        "\n" +
+                        "- Подходить близко к вольерам и гладить собак через сетку на выгулах. Животные могут быть агрессивны!\n" +
+                        "\n" +
+                        "- Кричать, размахивать руками, бегать между будками или вольерами, пугать и дразнить животных.\n" +
+                        "\n" +
+                        "- Посещение приюта для детей дошкольного и младшего школьного возраста без сопровождения взрослых.\n" +
+                        "\n" +
+                        "- Нахождение на территории приюта детей среднего и старшего школьного возраста без  сопровождения взрослых или письменной справки-разрешения от родителей или законных представителей.\n" +
+                        "\n" +
+                        "- Посещение приюта в состоянии алкогольного, наркотического опьянения.\n" +
+                        " Возврат в предыдущее меню (/back)\n" +
+                        " Выключить бота (/stop)";
 
-        if (currentState == SHELTER_INFO) {
-            String responseText = "Находясь на территории приюта, пожалуйста, соблюдайте наши правила и технику безопасности!\n" +
-                    "\n" +
-                    "                ЗАПРЕЩАЕТСЯ:\n" +
-                    "\n" +
-                    "- Самостоятельно открывать выгулы и вольеры без разрешения работника приюта.\n" +
-                    "\n" +
-                    "- Кормить животных. Этим Вы можете спровоцировать драку. \n" +
-                    "\n" +
-                    "- Оставлять после себя мусор на территории приюта и прилегающей территории.\n" +
-                    "\n" +
-                    "- Подходить близко к вольерам и гладить собак через сетку на выгулах. Животные могут быть агрессивны!\n" +
-                    "\n" +
-                    "- Кричать, размахивать руками, бегать между будками или вольерами, пугать и дразнить животных.\n" +
-                    "\n" +
-                    "- Посещение приюта для детей дошкольного и младшего школьного возраста без сопровождения взрослых.\n" +
-                    "\n" +
-                    "- Нахождение на территории приюта детей среднего и старшего школьного возраста без  сопровождения взрослых или письменной справки-разрешения от родителей или законных представителей.\n" +
-                    "\n" +
-                    "- Посещение приюта в состоянии алкогольного, наркотического опьянения.\n" +
-                    " Возврат в предыдущее меню (/back)\n" +
-                    " Выключить бота (/stop)";
-
-            SendMessage message = new SendMessage(chatId.toString(), responseText);
-            telegramBot.execute(message);
-        } else {
-            String responseText = "Для использования бота введите команду /start";
-            SendMessage message = new SendMessage(chatId.toString(), responseText);
-            telegramBot.execute(message);
+                SendMessage message = new SendMessage(chatId.toString(), responseText);
+                telegramBot.execute(message);
+            } else {
+                String responseText = "Для использования бота введите команду /start";
+                SendMessage message = new SendMessage(chatId.toString(), responseText);
+                telegramBot.execute(message);
+            }
+        } catch (ChatStateNotFoundException e) {
+            logger.warn("Caught exception in SafetyCommandHandler" + e.getMessage());
         }
+
 
     }
 
