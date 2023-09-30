c5746b1e8a128934b4d81ff468e3bc95d430b98c
DmitriiVolkovIzh <booyakaa@mail.ru>
2023-09-30 22:49:20 +0400

    Тесты на utils

--- src/test/java/pro/sky/AnimalShelter/utils/JsonMapConverterTest.java
+++ src/test/java/pro/sky/AnimalShelter/utils/JsonMapConverterTest.java
@@ -3,46 +3,139 @@ package pro.sky.AnimalShelter.utils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pro.sky.AnimalShelter.enums.BotCommand;
 import pro.sky.AnimalShelter.enums.UserReportStates;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
 @ExtendWith(MockitoExtension.class)
 public class JsonMapConverterTest {
 
+    @InjectMocks
+    private JsonMapConverter jsonMapConverter;
+
     @Test
-    @DisplayName("Проверяет, что объект типа Map<Long, Deque<BotCommand>> успешно преобразуется в JSON-строку")
+    @DisplayName("Тест на успешное преобразование объекта в JSON-строку:")
     public void testToCommandStatesJson() {
-        JsonMapConverter converter = new JsonMapConverter();
-        Map<Long, Deque<BotCommand>> map = new HashMap<>();
-        Deque<BotCommand> deque = new ArrayDeque<>();
-        deque.add(BotCommand.START);
-        deque.add(BotCommand.HELP);
-        map.put(123456789L, deque);
-        String expectedJson = "{\"123456789\":[\"START\",\"HELP\"]}";
-        String actualJson = converter.toCommandStatesJson(map);
-        assertEquals(expectedJson, actualJson);
+        Map<Long, Deque<BotCommand>> commandStates = new HashMap<>();
+        Deque<BotCommand> commandDeque = new LinkedList<>();
+        commandDeque.add(BotCommand.DOG);
+        commandStates.put(1L, commandDeque);
+        String expectedJson = "{\"1\":[\"DOG\"]}";
+        String json = jsonMapConverter.toCommandStatesJson(commandStates);
+        assertNotNull(json);
+        assertEquals(expectedJson, json);
     }
-
 
     @Test
-    @DisplayName("Проверяет, что объект типа Map<Long, Deque<UserReportStates>> успешно преобразуется в JSON-строку")
-    public void testToUserReportStatesJson() {
-        JsonMapConverter converter = new JsonMapConverter();
-        Map<Long, Deque<UserReportStates>> map = new HashMap<>();
-        Deque<UserReportStates> deque = new ArrayDeque<>();
-        deque.add(UserReportStates.PHOTO);
-        deque.add(UserReportStates.RATION);
-        map.put(123456789L, deque);
-        String expectedJson = "{\"123456789\":[\"PHOTO\",\"RATION\"]}";
-        String actualJson = converter.toUserReportStatesJson(map);
-        assertEquals(expectedJson, actualJson);
+    @DisplayName("Тест на успешное преобразование JSON-строки в объект:")
+    public void testToCommandStatesMap() {
+        // Arrange
+        String json = "{\"1\":[\"DOG\"]}";
+        Map<Long, Deque<BotCommand>> expectedCommandStates = new HashMap<>();
+        Deque<BotCommand> commandDeque = new LinkedList<>();
+        commandDeque.add(BotCommand.DOG);
+        expectedCommandStates.put(1L, commandDeque);
+        Map<Long, Deque<BotCommand>> commandStates = jsonMapConverter.toCommandStatesMap(json);
+        assertNotNull(commandStates);
+        assertEquals(expectedCommandStates, commandStates);
     }
 
+    @Test
+    @DisplayName("Тест на успешное преобразование объекта в JSON-строку для другого типа данных:")
+    public void testToUserReportStatesJson() {
+        Map<Long, Deque<UserReportStates>> userReportStates = new HashMap<>();
+        Deque<UserReportStates> stateDeque = new LinkedList<>();
+        stateDeque.add(UserReportStates.PHOTO);
+        userReportStates.put(1L, stateDeque);
+        String expectedJson = "{\"1\":[\"PHOTO\"]}";
+        String json = jsonMapConverter.toUserReportStatesJson(userReportStates);
+        assertNotNull(json);
+        assertEquals(expectedJson, json);
+    }
+
+    @Test
+    @DisplayName("Тест на успешное преобразование JSON-строки в объект для другого типа данных:")
+    public void testToUserReportStatesMap() {
+        // Arrange
+        String json = "{\"1\":[\"RATION\"]}";
+        Map<Long, Deque<UserReportStates>> expectedUserReportStates = new HashMap<>();
+        Deque<UserReportStates> stateDeque = new LinkedList<>();
+        stateDeque.add(UserReportStates.RATION);
+        expectedUserReportStates.put(1L, stateDeque);
+        Map<Long, Deque<UserReportStates>> userReportStates = jsonMapConverter.toUserReportStatesMap(json);
+        assertNotNull(userReportStates);
+        assertEquals(expectedUserReportStates, userReportStates);
+    }
+
+    @Test
+    @DisplayName("Тестирование преобразования пустого значения в JSON-строку и обратно")
+    void testToCommandStatesEmptyJson() {
+        Map<Long, Deque<BotCommand>> commandStates = Collections.emptyMap();
+        String expectedJson = "{}";
+        String json = jsonMapConverter.toCommandStatesJson(commandStates);
+        Map<Long, Deque<BotCommand>> parsedCommandStates = jsonMapConverter.toCommandStatesMap(json);
+        assertNotNull(json);
+        assertEquals(expectedJson, json);
+        assertNotNull(parsedCommandStates);
+        assertTrue(parsedCommandStates.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Тестирование преобразования JSON-строки в null")
+    void testToCommandStatesMapNull() {
+        String json = null;
+        Map<Long, Deque<BotCommand>> commandStates = jsonMapConverter.toCommandStatesMap(json);
+        assertNotNull(commandStates);
+        assertTrue(commandStates.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Тестирование ошибочного формата JSON-строки")
+    void testToCommandStatesMapMalformedJson() {
+        String malformedJson = "{invalid_json}";
+        Map<Long, Deque<BotCommand>> parsedCommandStates = jsonMapConverter.toCommandStatesMap(malformedJson);
+        assertNotNull(parsedCommandStates);
+        assertTrue(parsedCommandStates.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Тестирование преобразования JSON-строки, содержащей пустую коллекцию:")
+    void testToUserReportStatesMapEmpty() {
+        String json = "{\"1\": []}";
+        Map<Long, Deque<UserReportStates>> expectedMap = new HashMap<>();
+        Deque<UserReportStates> emptyDeque = new LinkedList<>();
+        expectedMap.put(1L, emptyDeque);
+        Map<Long, Deque<UserReportStates>> actualMap = jsonMapConverter.toUserReportStatesMap(json);
+        assertNotNull(actualMap);
+        assertEquals(expectedMap, actualMap);
+    }
+
+    @Test
+    @DisplayName("Тестирование случая, когда исходный объект для преобразования пустой")
+    void testToCommandStatesJsonEmptyObject() {
+        Map<Long, Deque<BotCommand>> commandStates = Collections.emptyMap();
+        String expectedJson = "{}";
+        String json = jsonMapConverter.toCommandStatesJson(commandStates);
+        assertNotNull(json);
+        assertEquals(expectedJson, json);
+    }
+
+    @Test
+    @DisplayName("Тестирование случая, когда JSON-строка содержит пустой массив")
+    void testToUserReportStatesMapEmptyArray() {
+        // Arrange
+        String json = "{\"1\": []}";
+        Map<Long, Deque<UserReportStates>> expectedMap = new HashMap<>();
+        Deque<UserReportStates> emptyDeque = new LinkedList<>();
+        expectedMap.put(1L, emptyDeque);
+        Map<Long, Deque<UserReportStates>> actualMap = jsonMapConverter.toUserReportStatesMap(json);
+        assertNotNull(actualMap);
+        assertEquals(expectedMap, actualMap);
+    }
 }
+
